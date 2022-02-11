0b2873888b291ae16a6b487e9995768e029e8a22
Alexander <example@gmail.com>
2022-02-11 13:42:14 +0300

    main functions are added

--- js/mainScript.js
+++ js/mainScript.js
@@ -6,7 +6,8 @@ const
     inputField = document.getElementById("input_field"),
     textArea = document.getElementById("textarea"),
     operationButtons = document.querySelectorAll(".operation"),
-
+    equationButton = document.getElementById("="),
+    reverseButton = document.getElementById("+/-"),
     fractionButton = document.getElementById("1/x"),
     factorialButton = document.getElementById("!"),
     powerButton = document.getElementById("x^2"),
@@ -29,7 +30,6 @@ digitButtons.forEach(element => {
         modifyText(element.value);
     });
 });
-
 operationButtons.forEach(element => {
     element.addEventListener("click", () => {
         clear();
@@ -39,7 +39,11 @@ operationButtons.forEach(element => {
         deleteText();
     });
 });
-
+equationButton.addEventListener("click", () => {
+    modifyArea(inputField.value, equationButton.value);
+    equation(inputField.value);
+    deleteText();
+});
 fractionButton.addEventListener("click", () => {
     fraction(inputField.value);
     deleteText();
@@ -56,14 +60,19 @@ factorialButton.addEventListener("click", () => {
     textArea.value = factorial(inputField.value);
     deleteText();
 });
+reverseButton.addEventListener("click", () => {
+    reverse(inputField.value);
+});
 
-
+/**
+ * Helping functions
+ */
 function modifyText(text) {
     inputField.value += `${text}`;
 }
 
 function modifyArea(text, operation) {
-    textArea.value += `${text} \n ${operation}`;
+    textArea.value += `${text} \n${operation}\n`;
 }
 
 function deleteText() {
@@ -72,39 +81,66 @@ function deleteText() {
 
 function clear() {
     textArea.value = '';
-    tempValue = '';
-    operation = '';
+    tempValue = null;
+    operation = null;
 }
 
-function equation( /**нужен аргумент который бюудет вторым аргументов во всех двух-аргументных функциях */ ) {
-
-    tempValue = '';
-    operation = '';
+function equation(secondValue) {
+    if (operation == '+') {
+        summ(tempValue, secondValue);
+    }
+    if (operation == '-') {
+        substract(tempValue, secondValue);
+    }
+    if (operation == 'x') {
+        multiply(tempValue, secondValue);
+    }
+    if (operation == '/') {
+        divide(tempValue, secondValue);
+    }
+    if (operation == '%') {
+        percent(tempValue, secondValue);
+    }
+    tempValue = null;
+    operation = null;
 }
 /**
  *Math functions
  */
 function summ(a, b) {
-    textArea.value = `${a} \n +`;
-    deleteText();
-    return a + b;
+    result = parseFloat(a) + parseFloat(b);
+    textArea.value += `${result}`;
 }
 
 function substract(a, b) {
-    textArea.value = `${a} \n -`;
-    return a - b;
+    if (a == 1000 && b == 7) {
+        showGhoul();
+    }
+    result = parseFloat(a) - parseFloat(b);
+    textArea.value += `${result}`;
 }
 
 function multiply(a, b) {
-    return a * b;
+    result = parseFloat(a) * parseFloat(b);
+    textArea.value += `${result}`;
 }
 
 function divide(a, b) {
-    return a / b;
+    result = parseFloat(a) / parseFloat(b);
+    textArea.value += `${result}`;
+}
+
+function percent(a, b) {
+    if (a > 0 && b > 0) {
+        result = parseFloat(a) * (parseFloat(b) / 100);
+        textArea.value += `${result}`;
+    }
 }
 
 function squareRoot(a) {
-    textArea.value = Math.sqrt(a);
+    if (a > 0) {
+        textArea.value = Math.sqrt(a);
+    }
 }
 
 function power(a) {
@@ -112,29 +148,32 @@ function power(a) {
 }
 
 function fraction(a) {
-    if (a != '') {
+    if (a != null) {
         textArea.value = 1 / a;
-    } else {
-        alert("Please throw me some numbers...");
-    }
-
+    } 
 }
 
 function factorial(a) {
-    if (a === 0) {
-        return 1;
-    } else {
-        return a * factorial(a - 1);
+    if (a != null) {
+        if (a === 0) {
+            return 1;
+        } else {
+            return a * factorial(a - 1);
+        }
     }
 }
 
-function percent(a, b) {
-    return;
+function reverse(input) {
+    input[0] == '-' ? input = input.substring(1) : input = '-' + input;
+    inputField.value = input;
 }
 
-function show() {
+function showGhoul() {
     image.removeAttribute("hidden");
+    setTimeout(() => {
+        hide();
+    }, 40);
 }
 function hide() {
-    image.setAttribute("hidden");
+    image.setAttribute("hidden", true);
 }
